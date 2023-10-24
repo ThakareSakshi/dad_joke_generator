0e016b735b9dd567d59d3b7aeea11ff9b9f9dd9a
ThakareSakshi <sakshithakare121@gmail.com>
2023-10-24 16:22:20 +0530

    uploaded

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 let dad_joke_content=document.querySelector(".dad_joke_content");
 const tell_me_joke_btn=document.querySelector(".tell_me_joke_btn");
+let Loading=document.querySelector(".wrapper");
+
 
 
 tell_me_joke_btn.addEventListener("click",()=>{
@@ -7,7 +9,9 @@ tell_me_joke_btn.addEventListener("click",()=>{
 })
 
 async function joke_Generator(){
-    dad_joke_content.innerText="loading..."
+    dad_joke_content.innerHTML="";
+    Loading.style.display="block";
+    
     let response=await fetch("https://api.api-ninjas.com/v1/dadjokes?limit=",{
         method: "GET",
         headers: {
@@ -17,7 +21,10 @@ async function joke_Generator(){
     });
     let joke = await response.json();
     // console.log(joke[0].joke)
-    dad_joke_content.innerText=joke[0].joke+"😂";
+    setTimeout(()=>{
+        Loading.style.display="none";
+        dad_joke_content.innerText=joke[0].joke+"😂";
+    },2000)
 
 
 }
